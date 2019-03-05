9ee34fd15c767f6800b494ff23fcd6c61e2e6a8a
Vince Carey <stvjc@channing.harvard.edu>
2019-03-05 03:52:18 -0500

    adding rmarkdown

--- vjcinst.sh
+++ vjcinst.sh
@@ -1,1 +1,1 @@
-R -e "BiocManager::install(c('BiocFileCache', 'SingleCellExperiment', 'org.Mm.eg.db', 'scater', 'TxDb.Mmusculus.UCSC.mm10.ensGene'))"
+R -e "BiocManager::install(c('BiocFileCache', 'SingleCellExperiment', 'org.Mm.eg.db', 'scater', 'TxDb.Mmusculus.UCSC.mm10.ensGene', 'rmarkdown'))"
